c6a2d50a669813d990f28c4f83596a3297d508e9
funkiben <funkiben@gmail.com>
2017-05-10 20:44:07 -0400

    Successfully sent test message to client

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 var net = require("net");
 
 var server = net.createServer(function (socket) {
-	console.log("client connected from " + socket.address());
+	console.log("client connected from " + socket.address().toString());
 	
 	socket.on("end", function() {
 		console.log("client disconnected: " + socket.address());
 	});
 	
-	socket.write("Hello");
+	//socket.write("Hello");
+	
+	var msg = "Hello World!";
+	
+	var buf = Buffer.allocUnsafe(14);
+	
+	buf.writeInt8(1, 0);
+	buf.writeInt8(12, 1);
+	buf.write(msg, 2, 12, 'utf8');
+	
+	socket.write(buf);
 	
 	socket.on("data", function(data) {
 		console.log(data);
